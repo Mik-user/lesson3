871294f72d4a4484eca20cd5e09a137b75b53dc3
Mik-user <mikmikh@yandex.ru>
2024-06-28 10:44:44 +0300

    Add files via upload

---
+++ Lesson 3.py
@@ -0,0 +1,5 @@
+#Практическое заанятие по теме "ПЕРЕМЕННЫЕ"
+summ_DZ=12
+sum_time=1.5
+name_course='Python'
+print('Курс:',name_course,', всего задач:',summ_DZ,', затрачено часов:',sum_time,', среднее время выполнения задания:',sum_time/summ_DZ)
